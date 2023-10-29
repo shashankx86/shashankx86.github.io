ac4bf5b8f8d021cbba05941e7116e069e65d3a91
Shashank Shekhar Pandey <64682801+shashankx86@users.noreply.github.com>
2023-10-29 10:10:20 +0000

    presentation.ts: remove "👋"

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -13,7 +13,7 @@ type Presentation = {
 
 const presentation: Presentation = {
   mail: "shashankspis20@gmail.com",
-  title: "Hi, I’m shashankx86 👋",
+  title: "Hi, I’m shashankx86",
   profile: "https://avatars.githubusercontent.com/u/64682801?v=4",
   description:
     "Namaste world, I'm a high school student, passionate about *Programming, Embedded system, Linux, and Everything*. I am currently learning *hardware hacking*. Anime, Manga and Manhwa <3.",
