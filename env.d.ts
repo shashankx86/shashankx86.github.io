9baca0623c20e0b24b74738793fc7d8cc0a179bc
shashankx86 <shashankspis20@gmail.com>
2025-02-10 09:07:17 +0530

    meta++

--- env.d.ts
+++ env.d.ts
@@ -4,3 +4,12 @@ declare module '*.json' {
   const value: any
   export default value
 }
+
+// Add router meta type
+import 'vue-router'
+
+declare module 'vue-router' {
+  interface RouteMeta {
+    title: string
+  }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -6,24 +6,43 @@ const router = createRouter({
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/HomeView.vue')
+      component: () => import('../views/HomeView.vue'),
+      meta: {
+        title: '0x5P | Home'
+      }
     },
     {
       path: '/projects',
       name: 'projects',
-      component: () => import('../views/ProjectsView.vue')
+      component: () => import('../views/ProjectsView.vue'),
+      meta: {
+        title: '0x5P | Projects'
+      }
     },
     {
       path: '/blog',
       name: 'blog',
-      component: () => import('../views/BlogView.vue')
+      component: () => import('../views/BlogView.vue'),
+      meta: {
+        title: '0x5P | Blog'
+      }
     },
     {
       path: '/:pathMatch(.*)*',
       name: 'not-found',
-      component: () => import('../views/NotFoundView.vue')
+      component: () => import('../views/NotFoundView.vue'),
+      meta: {
+        title: '0x5P | 404 Not Found'
+      }
     }
   ]
+})
+
+// Add navigation guard to handle title changes
+router.beforeEach((to, from, next) => {
+  // Update page title
+  document.title = to.meta.title as string || '0x5P'
+  next()
 })
 
 export default router
